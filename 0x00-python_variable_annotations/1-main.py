4319d25878cb7f2d1c08a53258dab1e73e0c310b
PORTJUSTO1@GMAIL.COM <portjusto1@gmail.com>
2023-03-30 23:15:17 +0100

    1

--- 0x00-python_variable_annotations/1-main.py
+++ 0x00-python_variable_annotations/1-main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-concat = __import__('1-concat').concat
+'''Task 1's module.
+'''
 
-str1 = "egg"
-str2 = "shell"
 
-print(concat(str1, str2) == "{}{}".format(str1, str2))
-print(concat.__annotations__)
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings.
+    '''
+    return str1 + str2
